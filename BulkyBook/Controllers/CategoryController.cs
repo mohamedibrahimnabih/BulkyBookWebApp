36d974bd0395cf5ea179df5834e140b255367501
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2024-06-12 21:51:41 +0300

    Create Category Controller and Index Action & View

---
+++ BulkyBook/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BulkyBook.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var listOfCategories = context.Categories.ToList();
+
+            return View(listOfCategories);
+        }
+    }
+}
