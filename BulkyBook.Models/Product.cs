b5ad957428aa65046186c2501509d81a199c871c
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2024-06-21 16:19:19 +0300

    Add Product Model and Table

---
+++ BulkyBook.Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string ISBN { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public string? Description { get; set; }
+
+        [DisplayName("List Price")]
+        [Range(1, 1000)]
+        public double ListPrice { get; set; }
+
+        [DisplayName("Price For 1-50")]
+        [Range(1, 1000)]
+        public double Price { get; set; }
+
+        [DisplayName("Price For 50+")]
+        [Range(1, 1000)]
+        public double Price50 { get; set; }
+
+        [DisplayName("Price For 100+")]
+        [Range(1, 1000)]
+        public double Price100 { get; set; }
+    }
+}
