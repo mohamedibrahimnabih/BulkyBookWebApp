2bd336af7958ed748f57352a2f5bef79455a14b3
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2024-06-21 21:21:37 +0300

    Product CRUD Operations

---
+++ BulkyBook/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,81 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index() => View(unitOfWork.ProductRepository.GetAll());
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                unitOfWork.ProductRepository.Add(product);
+                unitOfWork.Commit();
+
+                TempData["alert"] = "Added successfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var product = unitOfWork.ProductRepository.GetOne(e => e.Id == id);
+
+            return product != null ? View(product) : NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                unitOfWork.ProductRepository.Update(product);
+                unitOfWork.Commit();
+
+                TempData["alert"] = "Edited successfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = unitOfWork.ProductRepository.GetOne(e => e.Id == id);
+
+            if (product != null)
+            {
+                unitOfWork.ProductRepository.Remove(product);
+                unitOfWork.Commit();
+
+                TempData["alert"] = "Deleted successfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return NotFound();
+        }
+    }
+}
