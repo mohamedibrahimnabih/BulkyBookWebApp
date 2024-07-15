5cb275e7238bca1d7799ea4c73ccd99ab4a589ef
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2024-07-15 16:45:16 +0300

    Company CRUD Operation

---
+++ BulkyBook/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,83 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = StaticData.Role_Admin)]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index() => View(unitOfWork.CompanyRepository.GetAll());
+
+        public IActionResult UpSert(int? id)
+        {
+            Company? company = new Company();
+            if (id != null)
+            {
+                company = unitOfWork.CompanyRepository.GetOne(e => e.Id == id);
+            }
+
+            return company != null ? View(company) : NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpSert(Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                if(company.Id == 0)
+                {
+                    unitOfWork.CompanyRepository.Add(company);
+
+                    TempData["alert"] = "Added successfully";
+                }
+                else
+                {
+                    unitOfWork.CompanyRepository.Update(company);
+
+                    TempData["alert"] = "Edited successfully";
+                }
+
+                unitOfWork.Commit();
+                return RedirectToAction("Index");
+            }
+
+            return View(company);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var company = unitOfWork.CompanyRepository.GetOne(e => e.Id == id);
+
+            if (company != null)
+            {
+                unitOfWork.CompanyRepository.Remove(company);
+                unitOfWork.Commit();
+
+                TempData["alert"] = "Deleted successfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return NotFound();
+        }
+
+        #region APIs
+        [HttpGet]
+        public IActionResult GetAll() => Json(unitOfWork.CompanyRepository.GetAll());
+        #endregion
+    }
+}
---
+++ BulkyBook/wwwroot/js/company.js
@@ -0,0 +1,48 @@
+﻿$(document).ready(function () {
+    $('#tableData').DataTable({
+        ajax: {
+            url: '/Admin/Company/GetAll',
+            dataSrc: ''
+        },
+        columns: [
+            { data: 'name', defaultContent: 'N/A' },
+            { data: 'streetAddress', defaultContent: 'N/A' },
+            { data: 'city', defaultContent: 'N/A' },
+            { data: 'state', defaultContent: 'N/A' },
+            { data: 'postalCode', defaultContent: 'N/A' },
+            { data: 'phoneNumber', defaultContent: 'N/A' },
+            {
+                data: null,
+                className: 'dt-center',
+                defaultContent: `
+                                    <a href="" class="btn btn-warning btn-sm edit-button"><i class="bi bi-pencil-square"></i> Edit</a>
+                                    <button class="btn btn-danger btn-sm delete-button" data-bs-toggle="modal" data-bs-target="#deleteModal"><i class="bi bi-trash"></i> Delete</button>
+                                `,
+                orderable: false
+            }
+        ]
+    });
+
+    // Handle edit button click
+    $('#tableData').on('click', '.edit-button', function (e) {
+        e.preventDefault();
+        var data = $('#tableData').DataTable().row($(this).parents('tr')).data();
+        var companyId = data.id;
+
+        // Redirect to the edit page
+        window.location.href = '/Admin/Company/UpSert/' + companyId;
+    });
+
+    // Handle delete button click
+    $('#tableData').on('click', '.delete-button', function () {
+        var data = $('#tableData').DataTable().row($(this).parents('tr')).data();
+        var companyId = data.id;
+        var companyName = data.name;
+
+        var deleteForm = document.getElementById('deleteForm');
+        deleteForm.action = '/Admin/Company/Delete/' + companyId;
+
+        var companyNameSpan = document.getElementById('companyName');
+        companyNameSpan.textContent = companyName;
+    });
+});
